bdcf836cd5d96a6f62835d84b05c1f43c4aa854b
Wone11 <gelayegeremew@gmail.com>
2022-10-20 15:28:12 +0300

    Image Filter added

---
+++ Image_Filter.py
@@ -0,0 +1,14 @@
+from PIL import Image,ImageFilter
+
+'''
+Reading an image given
+'''
+def main():
+    file_name='Google.png'
+    image=Image.open(file_name)
+    image.filter(ImageFilter.EDGE_ENHANCE_MORE).show()
+    
+    del image
+
+if __name__ == '__main__':
+    main()
