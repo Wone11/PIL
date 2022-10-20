70bfe38bc15a1368ac359000fb3cff666fd1b69c
Wone11 <gelayegeremew@gmail.com>
2022-10-20 11:10:07 +0300

    image created by using pil library

---
+++ PILS.py
@@ -0,0 +1,18 @@
+from turtle import width
+from PIL import Image
+from pyparsing import White
+
+
+'''
+main function here! create a png image for the given size and color 
+'''
+def main():
+    size = width,height = 320,240;
+    image = Image.new("RGB", size , 'White')
+    image.show()
+    
+    del image
+    
+#call the functions!
+if __name__ == '__main__':
+    main()
