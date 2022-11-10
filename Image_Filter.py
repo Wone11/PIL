795084a08866d7618043894ab473d3e2993e57a0
Wone11 <gelayegeremew@gmail.com>
2022-11-10 11:47:34 +0100

    image processing fileter functions done

--- Image_Filter.py
+++ Image_Filter.py
@@ -7,7 +7,7 @@ def main():
     file_name='Google.png'
     image=Image.open(file_name)
     image.filter(ImageFilter.EDGE_ENHANCE_MORE).show()
-    
+    # image.save()
     del image
 
 if __name__ == '__main__':
